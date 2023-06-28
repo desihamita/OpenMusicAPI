1833f9dc7e2c5282e5ad77241aa98bb9486ca1f6
desihamita <69859667+desihamita@users.noreply.github.com>
2023-06-28 16:27:37 +0700

    upload data album hapi plugin dan data validation

---
+++ src/server.js
@@ -0,0 +1,31 @@
+const Hapi = require('@hapi/hapi');
+const albums = require('./api/album');
+const AlbumsService = require('./services/inMemory/AlbumsService');
+const AlbumsValidator = require('./validator/albums');
+
+const init = async () => {
+  const albumsService = new AlbumsService();
+
+  const server = Hapi.server({
+    port: 5000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.register({
+    plugin: albums,
+    options: {
+      service: albumsService,
+      validator: AlbumsValidator,
+    },
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
